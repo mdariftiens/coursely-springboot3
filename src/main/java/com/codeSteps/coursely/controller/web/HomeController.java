56d2c3fcf0bc1f28aa01afa6d000f30792296ae3
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2025-11-05 15:26:19 +0530

    move files in web for web

--- src/main/java/com/codeSteps/coursely/controller/web/HomeController.java
+++ src/main/java/com/codeSteps/coursely/controller/web/HomeController.java
@@ -1,8 +1,7 @@
-package com.codeSteps.coursely.controller;
+package com.codeSteps.coursely.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
